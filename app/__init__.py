04500e8759251c3922bd799f0a1f0dbea3e14ca7
Omri Levy <omri.lvy@gmail.com>
2024-05-07 11:25:32 +0300

    Add origins to confing file

--- app/__init__.py
+++ app/__init__.py
@@ -6,12 +6,7 @@ from flask_cors import CORS
 from app.config import Config
 
 app = Flask(__name__)
-allow_origins =[
-    "http://localhost:3000",
-    "http://localhost:3001",
-    "http://127.0.0.1:3000",
-    "http://127.0.0.1:3001",
-]
+allow_origins = app.config['ORIGINS']
 app.config['MONGO_URI'] = Config.MONGO_URI
 CORS(app, origins=allow_origins)
 mongo = PyMongo(app)
--- app/config.py
+++ app/config.py
@@ -4,4 +4,5 @@ load_dotenv()
 
 class Config:
     MONGO_URI = os.getenv('MONGO_URI')
+    ORIGINS = [os.getenv('PROD_ORIGIN'), os.getenv('DEV_ORIGIN')]
 
