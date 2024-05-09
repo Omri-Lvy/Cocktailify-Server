a737ed39efa845874b654721fbb19c3f055c7ba6
Omri Levy <omri.lvy@gmail.com>
2024-05-09 11:02:17 +0300

    edit config file

--- app/config.py
+++ app/config.py
@@ -1,9 +1,10 @@
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
 
-class Config:
-    MONGO_URI = os.getenv('MONGO_URI')
-    PROD_ORIGIN = os.getenv('PROD_ORIGIN')
-    DEV_ORIGIN = os.getenv('DEV_ORIGIN')
 
+class Config:
+    MONGO_URI = os.getenv('MONGO_URI') or ''
+    PROD_ORIGIN = os.getenv('PROD_ORIGIN') or ''
+    DEV_ORIGIN = os.getenv('DEV_ORIGIN') or 'http://localhost:3000, http://localhost:3001'
